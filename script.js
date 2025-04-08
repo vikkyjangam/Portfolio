8fc7d76f1deafef7ac03236485d1d7c08ee993ff
VIGNANVARDHAN JANGAM <90438037+vikkyjangam@users.noreply.github.com>
2025-04-07 20:02:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer",  "Web Developer"],
+    strings: ["Software Developer",  "Application Developer"],
     typeSpeed: 100,
     backSpeed:100,
     backDelay: 1000,
